d48b9cce3645acab3181c1931c6f40e9fbf36709
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 15:29:15 -0300

    Terceiro requisito concluído

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(employeeName = 'vazio') {
+  if (employeeName === 'vazio') return {};
+  return data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
