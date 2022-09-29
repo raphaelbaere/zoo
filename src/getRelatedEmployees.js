a82020652371794cd155f50e2fc5a0a7053702d7
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 16:12:14 -0300

    Requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let isItManager = false;
+  data.employees.forEach((element) => {
+    if ((element.managers.some((element2) => element2 === id))) {
+      isItManager = true;
+    }
+  });
+  return isItManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const relatedEmployees = data.employees.filter((element) =>
+      element.managers.some((element2) => element2 === managerId));
+    return relatedEmployees.map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
