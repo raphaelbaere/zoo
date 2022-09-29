bae51b022be6d6f842b1999600bc182d358f69db
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 15:06:46 -0300

    Primeiro requisito concluído

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // Começando
+function getSpeciesByIds(...ids) {
+  const speciesArray = [];
+  if (ids.length < 1) {
+    return [];
+  }
+  ids.forEach((element) =>
+    speciesArray.push(data.species.find((element2) => element === element2.id)));
+  return speciesArray;
 }
-
 module.exports = getSpeciesByIds;
