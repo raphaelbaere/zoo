9bd2d7eb684d1fecd332bcc2036c4de7f0848186
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 19:30:33 -0300

    Requisito 10 concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const oldestFromFirstSpeciesObject = data.species.find((element) =>
+    element.id === data.employees.find((element2) =>
+      element2.id === id).responsibleFor[0]).residents.reduce((acc, curr) => {
+    if (acc.age < curr.age) {
+      return curr;
+    }
+    return acc;
+  });
+  return Object.values(oldestFromFirstSpeciesObject);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
