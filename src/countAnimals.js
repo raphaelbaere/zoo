81a090742776634b77ca495d6bc3ef3d03b394ca
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 17:15:30 -0300

    Requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals({ specie: empSpecie, sex: empSex } = 0) {
+  const objetoRetornado = {};
+  if (!empSpecie && !empSex) {
+    data.species.forEach((element) => {
+      objetoRetornado[element.name] = element.residents.reduce((acc) => acc + 1, 0);
+    });
+  } else if (empSpecie && !empSex) {
+    return data.species.filter((element) =>
+      element.name === empSpecie)[0].residents.reduce((acc) => acc + 1, 0);
+  } else {
+    return data.species.filter((element) =>
+      element.name === empSpecie)[0].residents.filter((element) =>
+      element.sex === empSex).reduce((acc) => acc + 1, 0);
+  }
+  return objetoRetornado;
 }
 
 module.exports = countAnimals;
