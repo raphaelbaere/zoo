150254d8909b82163d75a72b00c10b554a266814
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 18:12:50 -0300

    Requisito 8 concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const newObject = {
+    child: entrants.filter((element) => element.age < 18).reduce((acc) => acc + 1, 0),
+    adult: entrants.filter((element) =>
+      element.age >= 18 && element.age < 50).reduce((acc) => acc + 1, 0),
+    senior: entrants.filter((element) => element.age >= 50).reduce((acc) => acc + 1, 0),
+  };
+  return newObject;
+};
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = 0) {
+  if (entrants === 0 || entrants.length < 1 || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entriesByAge = countEntrants(entrants);
+  return (entriesByAge.child * data.prices.child)
+  + (entriesByAge.adult * data.prices.adult)
+  + (entriesByAge.senior * data.prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
