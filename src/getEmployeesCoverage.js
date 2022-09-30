02fc4ff98b950a7c0d648018b64c22a06386a326
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-29 21:31:58 -0300

    Requisito 11 concluído

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function validaId(id) {
+  if (!data.employees.some((element) => element.id === id)) {
+    throw new Error('Informações inválidas');
+  }
+  return true;
+}
+
+function validaName(name) {
+  if (!data.employees.some((element) => element.firstName === name || element.lastName === name)) {
+    throw new Error('Nome inválido');
+  }
+  return true;
+}
+
+function criaObjeto({ id: objId, firstName: objFN,
+  lastName: objLN, responsibleFor: objRPS }) {
+  const newObject = {
+    id: objId,
+    fullName: `${objFN} ${objLN}`,
+    species: objRPS.map((element) =>
+      data.species.find((element2) => element2.id === element).name),
+    locations: objRPS.map((element) =>
+      data.species.find((element2) => element2.id === element).location),
+  };
+  return newObject;
+}
+
+function getEmployeesCoverage({ name: empName, id: empId } = 0) {
+  if (!empName && !empId) {
+    const newArray = [];
+    data.employees.forEach((element) => newArray.push(criaObjeto(element)));
+    return newArray;
+  } if (empName && !empId) {
+    validaName(empName);
+    const employeeObject = data.employees.find((element) =>
+      element.firstName === empName || element.lastName === empName);
+    return criaObjeto(employeeObject);
+  }
+  validaId(empId);
+  const employeeObject = data.employees.find((element) => element.id === empId);
+  return criaObjeto(employeeObject);
 }
 
 module.exports = getEmployeesCoverage;
